f38776007baa40d8979026a878b16ec3063d2702
Diogo Silva <digax96@gmail.com>
2016-11-29 17:05:08 +0000

    Templates, index and config introduced

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php    
+    // load up your config file
+    require_once("resources/config.php");
+     
+    require_once(TEMPLATES_PATH . "/header.php");
+?>
+
+<?php
+    require_once(TEMPLATES_PATH . "/footer.php");
+?>
---
+++ resources/templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Home page</title>
+</head>
+
+<body>
+  <div id="header">
+    <h1>Header (Meter o nome do site)</h1>
+    
+    <ul class="nav global">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Articles</a></li>
+      <li><a href="#">Portfolio</a></li>
+    </ul>
