2555924c423e8b4e91677505af07e92c23d45a7e
Diogo Luís Silva <digax96@gmail.com>
2016-12-14 17:10:18 +0000

    Header changes

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -2,33 +2,43 @@
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <title>Home page</title>
+  	<meta charset="UTF-8">
+  	<title>Home page</title>
 </head>
 
 <body>
     <header>
-    <!--
-    <div id="fast search">
-      <input type="text" name="search" placeholder="Search restaurant..">
-      <select>
-        <option disabled selected value> -- select an option -- </option>
-        <option value="owner">Owner</option>
-        <option value="restaurant">Restaurant</option>
-        <option value="reviewer">Reviewer</option>
-      </select>
-      <button type="button">Search</button>
-    </div>
-    -->
-    <div class="logs">
-    <ul>
-      <li> <a href="#">Home</a></li>
-      <li> <a href="#">Restaurants</a></li>
-      <li> <a href="#">Top 5 Restaurants</a></li>
-      <li> <a href="#">About</a> </li>
-      <li> <a href="#">Login</a></li>
-      <li> <a href="#">Register</a></li>
-    </ul>
-    </div>
-
-  </header>
+    	<!--
+    	<div id="fast search">
+    	  <input type="text" name="search" placeholder="Search restaurant..">
+    	  <select>
+    	    <option disabled selected value> -- select an option -- </option>
+    	    <option value="owner">Owner</option>
+    	    <option value="restaurant">Restaurant</option>
+    	    <option value="reviewer">Reviewer</option>
+    	  </select>
+    	  <button type="button">Search</button>
+    	</div>
+    	-->
+    	<div class="logs">
+    		<ul>
+    	  		<li> <a href="#">Home</a></li>
+    	  		<li> <a href="#">Restaurants</a></li>
+    	  		<li> <a href="#">Top 5 Restaurants</a></li>
+    	  		<li> <a href="#">About</a> </li>
+    	  		<?php
+    	    		if (isset($_SESSION['id'])){
+				?>
+						<li> <a href="#">Profile</a></li>
+				<?php
+    	    		} else {
+				?>
+						<li> <a href="#">Login</a></li>
+    	  				<li> <a href="#">Register</a></li>
+				<?php
+    	    		}
+    	 		?>
+    	  		
+    		</ul>
+    	</div>
+	</header>
