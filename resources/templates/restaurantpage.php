dbccf5585825901d4bcb683551b13f86b2fd34fc
Ariana <up201404789@fe.up.pt>
2016-12-15 00:09:04 +0000

    restaurantpage

--- resources/templates/restaurantpage.php
+++ resources/templates/restaurantpage.php
@@ -7,17 +7,26 @@ include_once($config["paths"]["resources"]["api"]."restaurant.php");
 $rest = getRestaurants();
 
 foreach($rest as $r){
+echo '<div id="principal-info">';       
+echo '<h1>' . $r['name'] . '</h1>';
+echo '<ul>' . 'Informations:' . $r['inf'].'</ul>';
+echo '<ul>' . 'Phone Number:'. $r['phonenumber'] . '</ul>';
+echo '<ul>' . 'Average Cost:'. $r['averagecost'] . '</ul>';
+echo '<ul>' . 'Schedule:'. $r['schedule'] . '</ul>';
+echo '<ul>' . 'Adress:'. $r['adress'] . '</ul>';
+echo '<ul>' . 'Categories:'. $r['categories'] . '</ul>';
+echo '</div>';
+$image_path = "../Imagens/Owners/" . $restaurant_id . "/";
 
-     echo '<h1>' . $r['name'] . '</h1>';
-        echo '<h2>' . $r['inf'] . '</h2>';
-        echo '<h2>' . $r['phonenumber'] . '</h2>';
-        echo '<h2>' . $r['averagecost'] . '</h2>';
-        echo '<h2>' . $r['schedule'] . '</h2>';
-        echo '<h2>' . $r['adress'] . '</h2>';
-        echo '<h2>' . $r['categories'] . '</h2>';
+echo '<img src="' . $image_path '" alt="restaurant_pics">'
+
+echo '<button type="submit">More info</button>';
+
 }
 
 
 ?>
 
 </div>
+
+$image_path = "../database/images/" . $restaurant_id . "/";
