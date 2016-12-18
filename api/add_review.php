59930cb426b79ae0adbd37aca66ae7ccb840c1f3
Diogo Luís Silva <digax96@gmail.com>
2016-12-18 23:12:32 +0000

    Review fixed

--- api/add_review.php
+++ api/add_review.php
@@ -1,19 +1,13 @@
 <?php
 	session_start();
 	
-	include_once("restaurant.php");
+	include_once("connection.php");
 	include_once("review.php");
-	include_once("user.php");
 
 	$reviewtext = $_POST["comments"];
 	$restid = $_POST["restid"];
 	$userid = $_SESSION['id'];
 	$rating = $_POST["rating"];
-
-	echo $userid;
-	echo $reviewtext;
-	echo $restid;
-	echo $rating;
 
 	if($userid ==null){
 	}
