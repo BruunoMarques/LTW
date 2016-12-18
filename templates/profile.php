8a5fac9c0ec53d567800500a4bf15e056c718be8
Diogo Luís Silva <digax96@gmail.com>
2016-12-18 05:47:09 +0000

    Idented profile file and removed invalid tag

--- templates/profile.php
+++ templates/profile.php
@@ -13,63 +13,57 @@ $u =  getUserById($id)
 ?>
 
 <div id="user_informations">
-<a href="./?content=u_edit&id=<?=$id?>">
-<input type="Edit" value="Edit Profile!">
-</a>
+    <a href="./?content=u_edit&id=<?=$id?>">
+        <input type="Edit" value="Edit Profile!">
+    </a>
 
 
-<?php    
-echo '<h1>' . $u['firstname'] . " " . $u['lastname'] . '</h1>';
-?>
+    <?php    
+        echo '<h1>' . $u['firstname'] . " " . $u['lastname'] . '</h1>';
+    ?>
 
 
-<?php
-echo '<h4>' . 'Username:  '. '</h4>';
-echo '<ul>'. $u['username'] . '</ul>';
+    <?php
+        echo '<h4>' . 'Username:  '. '</h4>';
+    echo '<ul>'. $u['username'] . '</ul>';
 
-echo '<h4>' . 'Email: '. '</h4>';
-echo '<ul>' . $u['email']. '</ul>';
+    echo '<h4>' . 'Email: '. '</h4>';
+    echo '<ul>' . $u['email']. '</ul>';
 
-echo '<h2>' . 'My Restaurants:  '. '</h2>';
+    echo '<h2>' . 'My Restaurants:  '. '</h2>';
 
-echo '<h2>' . 'My Reviews:  '. '</h2>';
+    echo '<h2>' . 'My Reviews:  '. '</h2>';
 
-$rv = getUserReviewsById($id);
+    $rv = getUserReviewsById($id);
 
-foreach($rv as $review){
+    foreach($rv as $review){
 
-?>
-<form id="rev">
-<?php
-    $idR= $review['idRestaurant'];
-    $restaurant = getRestaurantById($idR);
-?>
-<div class="div">
-<?php
-echo '<h4>' . 'Restaurant: '.'</h4>';
-echo '<ul>' . $restaurant['name'] . '</ul>'; 
-?>
-</div>
-<div class="div">
-<?php
-echo '<h4>' . 'Rating: '.'</h4>';
-echo '<ul>' . $review['rating'] . '</ul>'; 
-?>
-</div>
-
-<div class="div">
-<?php
-echo '<h4>' . 'Description: '.'</h4>';
-echo '<ul>' . $review['info']  . '</ul>'; 
-?>
-</div>
-
-</form>
-
-<?php
-}
-?>
-
-</form>
-
+    ?>
+    <form id="rev">
+        <?php
+            $idR= $review['idRestaurant'];
+            $restaurant = getRestaurantById($idR);
+        ?>
+        <div class="div">
+            <?php
+                echo '<h4>' . 'Restaurant: '.'</h4>';
+                echo '<ul>' . $restaurant['name'] . '</ul>'; 
+            ?>
+        </div>
+        <div class="div">
+            <?php
+                echo '<h4>' . 'Rating: '.'</h4>';
+                echo '<ul>' . $review['rating'] . '</ul>'; 
+            ?>
+        </div>
+        <div class="div">
+            <?php
+                echo '<h4>' . 'Description: '.'</h4>';
+                echo '<ul>' . $review['info']  . '</ul>'; 
+            ?>
+        </div>
+    </form>
+    <?php
+        }
+    ?>
 </div>
