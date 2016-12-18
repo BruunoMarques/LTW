e376e77129ee2e776a54d6b7d503bd808b561324
Diogo Luís Silva <digax96@gmail.com>
2016-12-18 05:46:28 +0000

    Fixed existing user bug

--- api/registeruser.php
+++ api/registeruser.php
@@ -11,16 +11,16 @@ $confirm_password = $_POST["confirm_password"];
 $email = $_POST["email"];
 
 
-if (strlen($username) < 20 && strlen($firstname) < 20 && strlen($lastname) < 20){
+if (strlen($username) < 20 && strlen($firstname) < 20 && strlen($lastname) < 20) {
 	
 	if($password == $confirm_password && strlen($password) == strlen($confirm_password)){
 		
 		$pass = password_hash($password,PASSWORD_DEFAULT);
 		
-		if (getUserByUsername($username)){
+		if (!count(getUserByUsername($username))){
 			echo 'usr exists';
 		} else{
-			if (getUserByEmail($email)){
+			if (!count(getUserByEmail($email))){
 				echo 'email exists';
 			}
 			else{
